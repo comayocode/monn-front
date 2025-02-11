8e622b9d61dff930dbbf100c735f29e09d738c1d
Comayocode <ronalc726@gmail.com>
2025-02-10 21:03:26 -0500

    feat: Redesigned Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,85 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Usamos useNavigate para la redirección
-import { login } from '../services/authService'; // Importamos el servicio de login
+import { useNavigate } from 'react-router-dom';
+import { login } from '../services/authService';
+import './Login.css';
 
 function Login() {
+  const [isFocused, setFocusState] = useState({
+    username: false,
+    password: false,
+  });
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate(); // Usamos useNavigate
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const response = await login(username, password);
-
     if (response.success) {
-      navigate('/admin'); // Redirigimos al Panel de Administración si el login es exitoso
+      navigate('/admin');
     } else {
-      setError(response.message); // Mostramos un mensaje de error si las credenciales son incorrectas
+      setError(response.message);
     }
   };
 
   return (
-    <div className="login">
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Iniciar Sesión</button>
+    <div className='login'>
+      <h2 className='login__title'>Iniciar Sesión</h2>
+      <form className='login__form' onSubmit={handleSubmit}>
+        <div className='login__input-group'>
+          <label
+            className={`login__input-label ${
+              isFocused.username ? 'focused' : ''
+            }`}
+          >
+            Usuario
+          </label>
+          <input
+            className='login__input'
+            type='text'
+            placeholder='Escribe tu nombre de usuario'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onFocus={() =>
+              setFocusState((prev) => ({ ...prev, username: true }))
+            }
+            onBlur={() =>
+              setFocusState((prev) => ({ ...prev, username: false }))
+            }
+            required
+          />
+        </div>
+        <div className='login__input-group'>
+          <label
+            className={`login__input-label ${
+              isFocused.password ? 'focused' : ''
+            }`}
+          >
+            Contraseña
+          </label>
+          <input
+            className='login__input'
+            type='password'
+            placeholder='Escribe tu contraseña'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onFocus={() =>
+              setFocusState((prev) => ({ ...prev, password: true }))
+            }
+            onBlur={() =>
+              setFocusState((prev) => ({ ...prev, password: false }))
+            }
+            required
+          />
+        </div>
+        <button className='login__button' type='submit'>
+          Iniciar Sesión
+        </button>
       </form>
-      {error && <p className="error">{error}</p>}
+      <div className='login__message'>
+        {error && <p className='login__error'>{error}</p>}
+      </div>
     </div>
   );
 }
