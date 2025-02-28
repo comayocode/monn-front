9ff443a7882223da5b9d8c90cf3fc7f0e4020525
Comayocode <ronalc726@gmail.com>
2025-02-27 20:55:42 -0500

    feat(auth): add input validation and error handling in login form
    - Implemented input validation to prevent empty submissions.
    - Added error handling to display specific messages for missing or invalid credentials.
    - Improved input UI feedback by highlighting errors and dynamically removing error styles when the user modifies the input.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,13 +10,31 @@ function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [prevValues, setPrevValues] = useState({ username: '', password: '' }); // Guardar los valores antes de hacer login
+  const [emptyFields, setEmptyFields] = useState({
+    username: false,
+    password: false,
+  }); // Rastrear qué campos están vacíos en el último intento
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     setError('');
+
+    const newEmptyFields = {
+      username: !username.trim(),
+      password: !password.trim(),
+    }; // Detectar qué campos están vacíos
+    setEmptyFields(newEmptyFields);
+
+    if (newEmptyFields.username || newEmptyFields.password) {
+      setError('Por favor, completa todos los campos.');
+      return;
+    }
+
+    setPrevValues({ username, password }); // Guardamos los valores ingresados antes del intento
+
     const result = await login(username, password);
-    console.log(result);
     if (result.success) {
       navigate('/admin/dashboard');
     } else {
@@ -28,18 +46,28 @@ function Login() {
     <div className='login'>
       <form className='login__form' onSubmit={handleLogin}>
         <h2 className='login__title'>Iniciar Sesión</h2>
-        <p className='login__description'>Ingresa tus credenciales para continuar.</p>
+        <p className='login__description'>
+          Ingresa tus credenciales para continuar.
+        </p>
         <Input
           label='Usuario'
           placeholder='mi.usuario@correo.com'
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          error={
+            (!!error && username === prevValues.username) ||
+            emptyFields.username
+          }
         />
         <Input
           label='Contraseña'
           placeholder='••••••••••••'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          error={
+            (!!error && password === prevValues.password) ||
+            emptyFields.password
+          }
           variant='password'
         />
         <Button type='submit' variant='primary' size='normal' fullWidth>
